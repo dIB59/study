75ffb5a96d85116622216e301dc2cca0ee27a821
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-18 15:37:11 +0200

    spring init

---
+++ ByteSchoolApp/src/test/java/dev/byteschool/byteschoolapp/TestByteSchoolAppApplication.java
@@ -0,0 +1,11 @@
+package dev.byteschool.byteschoolapp;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestByteSchoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ByteSchoolAppApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
