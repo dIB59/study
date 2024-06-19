0d638ac621d1d167d4ed9c8e01197bba2a81a736
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-19 18:46:38 +0200

    chore: Update ChargeRequest to include email, token, amount, and description fields

--- ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/stripe/ChargeRequest.java
+++ ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/stripe/ChargeRequest.java
@@ -1,2 +1,3 @@
-package dev.byteschool.byteschoolapp.stripe.exception;public record ChargeRequest() {
-}
+package dev.byteschool.byteschoolapp.stripe;
+
+public record ChargeRequest(String email, String token, Long amount, String description) {}
