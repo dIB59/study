8a72223da6466163ae48d02591e92c0608281f83
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-19 18:47:15 +0200

    chore: Update StripeTokenDto to use 'token' instead of 'username' field

--- ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/stripe/StripeTokenDto.java
+++ ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/stripe/StripeTokenDto.java
@@ -5,36 +5,6 @@ public record StripeTokenDto(
         String expMonth,
         String expYear,
         String cvc,
-        String username
+        String token
 ) {
-    private String token;
-    private boolean success;
-
-    // Constructor to initialize all fields
-    public StripeTokenDto(String cardNumber, String expMonth, String expYear, String cvc, String username) {
-        this.cardNumber = cardNumber;
-        this.expMonth = expMonth;
-        this.expYear = expYear;
-        this.cvc = cvc;
-        this.username = username;
-        this.token = "";
-        this.success = false;
-    }
-
-    public String token() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public boolean success() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
 }
-
