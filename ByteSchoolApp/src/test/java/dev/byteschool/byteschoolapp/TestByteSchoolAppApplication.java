c16916de56c6cc8cea068799cc13180c4d13ee39
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-19 18:49:56 +0200

    chore: Refactor code

--- ByteSchoolApp/src/test/java/dev/byteschool/byteschoolapp/TestByteSchoolAppApplication.java
+++ ByteSchoolApp/src/test/java/dev/byteschool/byteschoolapp/TestByteSchoolAppApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 public class TestByteSchoolAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(ByteSchoolAppApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(ByteSchoolAppApplication::main)
+				.with(TestcontainersConfiguration.class)
+				.run(args);
 	}
 
 }
