78b3e8e6d65ac647a482c407c79e5aa44f6287c2
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-19 18:45:28 +0200

    chore: Add exception handling for Stripe-related errors in GlobalExceptionHandler

--- ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/GlobalExceptionHandler.java
+++ ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/GlobalExceptionHandler.java
@@ -1,2 +1,37 @@
-package dev.byteschool.byteschoolapp;public class GlobalExceptionHandler {
+package dev.byteschool.byteschoolapp;
+
+import com.stripe.exception.StripeException;
+import dev.byteschool.byteschoolapp.stripe.exception.StripeChargeNotFoundException;
+import dev.byteschool.byteschoolapp.stripe.exception.StripeCustomerNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+//@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(StripeCustomerNotFoundException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public String handleStripeProcessingException(StripeCustomerNotFoundException ex) {
+//        log.error(ex.getMessage());
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(StripeChargeNotFoundException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public String handleStripeChargeNotFoundException(Exception ex) {
+//        log.error(ex.getMessage());
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(StripeException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public String handleStripeException(Exception ex) {
+//        log.error(ex.getMessage());
+        return ex.getMessage();
+    }
 }
