09cf804a03a2f851cc3932285c6a94f19e1c66cb
Ibrahim <Ibrahim@ebs.net.pk>
2024-06-19 18:45:37 +0200

    chore: Refactor PaymentService interface and add Stripe-related methods

--- ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/PaymentService.java
+++ ByteSchoolApp/src/main/java/dev/byteschool/byteschoolapp/PaymentService.java
@@ -1,2 +1,16 @@
-package dev.byteschool.byteschoolapp.stripe;public interface PaymentService {
+package dev.byteschool.byteschoolapp;
+
+
+import dev.byteschool.byteschoolapp.stripe.StripeCardToken;
+import dev.byteschool.byteschoolapp.stripe.StripeTokenDto;
+
+    /**
+     ** methods should return id of Charge, Payment Intent and Customer
+     */
+public interface PaymentService {
+
+    String createCharge(String email, String token, Long amount, String description);
+    String createPaymentIntent(int amount, String currency);
+    String createCustomer(String email, String token);
+    StripeCardToken createToken(StripeTokenDto cardDto);
 }
